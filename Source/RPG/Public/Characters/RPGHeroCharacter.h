88c1c45c7c42abfb2a5710d5c919cd4e68498a44
WillTheWater <willthewater@protonmail.com>
2024-12-28 17:26:05 +0100

    Default Character Initial Setup

--- Source/RPG/Public/Characters/RPGHeroCharacter.h
+++ Source/RPG/Public/Characters/RPGHeroCharacter.h
@@ -1,17 +1,29 @@
 // WillTheWater All Rights Reserved
 
 #pragma once
-
 #include "CoreMinimal.h"
 #include "Characters/RPGBaseCharacter.h"
 #include "RPGHeroCharacter.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class RPG_API ARPGHeroCharacter : public ARPGBaseCharacter
 {
 	GENERATED_BODY()
 	
+public:
+	ARPGHeroCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+#pragma region Components
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+#pragma endregion
 };

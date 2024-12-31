668c009f82cb9d45f4f0e30c5fdbcb368580b7b8
WillTheWater <willthewater@protonmail.com>
2024-12-31 14:32:26 +0100

    AnimationBP and Blendspaces

--- Source/RPG/Private/AnimInstances/Hero/RPGHeroAnimInstance.cpp
+++ Source/RPG/Private/AnimInstances/Hero/RPGHeroAnimInstance.cpp
@@ -2,4 +2,30 @@
 
 
 #include "AnimInstances/Hero/RPGHeroAnimInstance.h"
+#include "Characters/RPGHeroCharacter.h"
 
+
+void URPGHeroAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	if (OwningCharacter)
+	{
+		OwningHeroCharacter = Cast<ARPGHeroCharacter>(OwningCharacter); 
+	}
+}
+
+void URPGHeroAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
+	if (bHasAcceleration)
+	{
+		ElapsedTime = 0.f;
+		bShouldEnterRelaxState = false;
+	}
+	else
+	{
+		ElapsedTime += DeltaSeconds;
+		bShouldEnterRelaxState = (ElapsedTime >= EnterRelaxedStateThreshold);
+	}
+}
--- Source/RPG/Public/AnimInstances/Hero/RPGHeroAnimInstance.h
+++ Source/RPG/Public/AnimInstances/Hero/RPGHeroAnimInstance.h
@@ -6,12 +6,28 @@
 #include "AnimInstances/RPGCharacterAnimInstance.h"
 #include "RPGHeroAnimInstance.generated.h"
 
-/**
- * 
- */
+class ARPGHeroCharacter;
+
 UCLASS()
 class RPG_API URPGHeroAnimInstance : public URPGCharacterAnimInstance
 {
 	GENERATED_BODY()
 	
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds);
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|References")
+	ARPGHeroCharacter* OwningHeroCharacter;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	bool bShouldEnterRelaxState;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float EnterRelaxedStateThreshold = 5.f;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "AnimData|LocomotionData")
+	float ElapsedTime;
+
 };

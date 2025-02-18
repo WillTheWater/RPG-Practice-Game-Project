2f47cfa22b0ff057ddec04c39dd3b96e2c9ee90d
WillTheWater <willthewater@protonmail.com>
2025-02-18 11:09:00 +0100

    Attributes Added

--- Source/RPG/Public/GAS/RPGAttributeSet.h
+++ Source/RPG/Public/GAS/RPGAttributeSet.h
@@ -4,14 +4,44 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "GAS/RPGAbilitySystemComponent.h"
 #include "RPGAttributeSet.generated.h"
 
-/**
- * 
- */
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
+
 UCLASS()
 class RPG_API URPGAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public:
 	
+	URPGAttributeSet();
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData CurrentHealth;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, CurrentHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MaxHealth)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData CurrentRage;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, CurrentRage)
+
+	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	FGameplayAttributeData MaxRage;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, MaxRage)
+
+	UPROPERTY(BlueprintReadOnly, Category="Damage")
+	FGameplayAttributeData AttackPower;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, AttackPower)
+
+	UPROPERTY(BlueprintReadOnly, Category="Damage")
+	FGameplayAttributeData DefensePower;
+	ATTRIBUTE_ACCESSORS(URPGAttributeSet, DefensePower)
 };
